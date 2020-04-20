5f500197381c450b2162d094a760a6de68e5ca2b
Georgios Chaniotakis <haniotinho@gmail.com>
2020-04-21 00:58:04 +0300

    video list styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({video}) =>{
     var videoObj = video.snippet;
     return (
-        <div>
-            <img src={videoObj.thumbnails.medium.url}/>
-            {videoObj.title}
+        <div className="video-item item">
+            <img src={videoObj.thumbnails.medium.url} className="ui image"/>
+            <div className="content">
+                <div className="header">
+                    {videoObj.title}
+                </div>
+            </div>            
         </div>
     );
 };
