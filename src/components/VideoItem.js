f3ea3a61f68ce894d61ec3369c579159e14bf48e
Georgios Chaniotakis <haniotinho@gmail.com>
2020-04-21 01:25:09 +0300

    selected video property

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({video}) =>{
+const VideoItem = ({video, onVideoSelect}) =>{
     var videoObj = video.snippet;
     return (
-        <div className="video-item item">
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
             <img src={videoObj.thumbnails.medium.url} className="ui image"/>
             <div className="content">
                 <div className="header">
